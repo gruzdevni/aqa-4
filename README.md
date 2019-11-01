f62c719c41cbab5add12a756612d0f8afee71eac
Nikolay Gruzdev <gruzdevni@mail.ru>
2019-11-02 02:29:01 +0300

    Dropdown test added, date issue fixed, README added

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/w6bv94ni77qmd38r?svg=true)](https://ci.appveyor.com/project/gruzdevni/aqa-4)
+
+
+# Automated test set for Bank subscription form
+### Just another test using Selenide.
+
+### To install it:
+1. Clone or download repository
+2. Open with IntelliJ IDEA or other IDE.
--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -3,22 +3,38 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class FormTest {
 
     @Test
-    void successfulFormFilling(){
+    void successfulFormFilling() {
         open("http://localhost:9999");
-        List <SelenideElement> fields = $$("[class='input__control']");
+        List<SelenideElement> fields = $$("[class='input__control']");
         fields.get(0).setValue("Владивосток");
         field1Clear(fields);
-        fields.get(1).setValue("05.11.2019");
+        fields.get(1).setValue(dateForVisit());
+        fields.get(2).setValue("Иван Петров-Водкин");
+        fields.get(3).setValue("+79012345678");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.matchText("брон")).click();
+        $(withText("Успешно")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void dropdownTest() {
+        open("http://localhost:9999");
+        List<SelenideElement> fields = $$("[class='input__control']");
+        fields.get(0).setValue("ро");
+        $(withText("Хабаровск")).click();
+        field1Clear(fields);
+        fields.get(1).setValue("09.11.2019");
         fields.get(2).setValue("Иван Петров-Водкин");
         fields.get(3).setValue("+79012345678");
         $("[data-test-id='agreement']").click();
@@ -30,4 +46,12 @@ public class FormTest {
         fields.get(1).sendKeys(Keys.CONTROL + "a");
         fields.get(1).sendKeys(Keys.DELETE);
     }
+
+    private String dateForVisit() {
+        LocalDateTime firstAvailableDate = LocalDateTime.now().plusDays(3);
+        DateTimeFormatter formatPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String firstAvailableDateFormatted = formatPattern.format(firstAvailableDate);
+        return firstAvailableDateFormatted;
+    }
+
 }
